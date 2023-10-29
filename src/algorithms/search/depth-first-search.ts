9fd86ad45ce8a04462f8416881a36b58fe47e1a5
Matamando Kalilani <matamandokalilani@outlook.com>
2023-10-29 19:09:09 +0200

    Save

---
+++ src/algorithms/search/depth-first-search.ts
@@ -0,0 +1,5 @@
+import { BinarySearchTree } from "../../data-structures/tree/binary-search-tree";
+
+const depthFirstSearch = (tree: BinarySearchTree<number>, item: number) => {};
+
+export { depthFirstSearch };
